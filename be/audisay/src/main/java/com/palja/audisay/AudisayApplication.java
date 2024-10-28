39201f9cb3604cbce0c88570abfff1f7d0c35ea6
yr5698 <yr5698@gmail.com>
2024-10-28 15:51:40 +0900

    be/feat: db 연결 없이 실행 되도록 설정 (#6)

--- be/audisay/src/main/java/com/palja/audisay/AudisayApplication.java
+++ be/audisay/src/main/java/com/palja/audisay/AudisayApplication.java
@@ -2,10 +2,10 @@ package com.palja.audisay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AudisayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AudisayApplication.class, args);
 	}
