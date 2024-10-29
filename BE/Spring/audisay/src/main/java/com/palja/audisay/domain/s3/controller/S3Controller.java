1bb97b98f2e87714f50dd4304a467b8d076d1f9e
yr5698 <yr5698@gmail.com>
2024-10-29 17:03:30 +0900

    be/feat: S3, 테스트 컨트롤러에 Swagger 추가 (#20)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/s3/controller/S3Controller.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/s3/controller/S3Controller.java
@@ -10,21 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.palja.audisay.domain.s3.dto.S3ResponseDto;
 import com.palja.audisay.domain.s3.service.S3Service;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/file")
 @RequiredArgsConstructor
+@Tag(name="S3 파일", description = "S3 파일 업로드/다운로드 API")
 public class S3Controller {
 
 	private final S3Service awsS3Service;
 
 	@GetMapping("/presigned/upload")
+	@Operation(summary = "파일 업로드", description = "구현 중")
 	public S3ResponseDto getPresignedUrlToUpload(@RequestParam(value = "filename") String fileName) throws IOException {
 		return awsS3Service.getPresignedUrlToUpload(fileName);
 	}
 
 	@GetMapping("/presigned/download")
+	@Operation(summary = "파일 다운로드", description = "3분 간 유효한 다운로드 링크 반환")
+	@Parameters({
+		@Parameter(name="filename", description = "파일 명", example="temp/audisay.png")
+	})
 	public S3ResponseDto getPresignedUrlToDownload(@RequestParam(value = "filename") String fileName) throws IOException {
 		return awsS3Service.getPresignedUrlToDownload(fileName);
 	}
--- BE/Spring/audisay/src/main/java/com/palja/audisay/global/controller/TestController.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/global/controller/TestController.java
@@ -1,18 +1,18 @@
 package com.palja.audisay.global.controller;
 
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.palja.audisay.domain.s3.dto.S3ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/test")
+@Tag(name="테스트", description = "테스트 API")
 public class TestController {
 	@GetMapping()
+	@Operation(summary = "Get 테스트", description = "작동되면 hello world가 반환되는 API")
 	public String test() {
 		return "Hello World";
 	}
