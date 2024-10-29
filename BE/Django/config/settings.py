fb1a5e4d0b324356f28e1046b9d6af2255d4ebdd
Elenaljh <hye9807@naver.com>
2024-10-29 15:10:33 +0900

    cicd: django STATIC_ROOT 설정 추가 #11

--- BE/Django/config/settings.py
+++ BE/Django/config/settings.py
@@ -18,7 +18,6 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
