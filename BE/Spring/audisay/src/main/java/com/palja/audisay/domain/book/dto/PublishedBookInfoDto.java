91136286a550bf890b9b7168b762539b1a602d22
김아름 <rdf136@naver.com>
2024-10-30 15:05:16 +0900

    be/feat: 출판 도서 상세 정보 DTO 구현 (#22)

---
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/dto/PublishedBookInfoDto.java
@@ -0,0 +1,68 @@
+package com.palja.audisay.domain.book.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.palja.audisay.domain.book.entity.Dtype;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 출판 도서 정보 Dto
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class PublishedBookInfoDto {
+	private Long bookId;
+	private String title;
+	private String cover;
+	private String category;
+	private String coverAlt;
+	private String author;
+	private String publisher;
+	private LocalDate publishedAt;
+	private String story;
+	private String isbn;
+	private Boolean myTtsFlag;
+	private Dtype dtype;
+	private ReviewDistribution reviewDistribution;
+	private MemberInfo memberInfo;
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	class ReviewDistribution {
+		private Float average;
+		@JsonProperty("5")
+		private Float five;
+		@JsonProperty("4")
+		private Float four;
+		@JsonProperty("3")
+		private Float three;
+		@JsonProperty("2")
+		private Float two;
+		@JsonProperty("1")
+		private Float one;
+	}
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	class MemberInfo {
+		private Boolean cartFlag;
+		private Boolean likedFlag;
+	}
+}
