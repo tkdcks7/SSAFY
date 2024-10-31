6f3c468664bfdca5e986504d942eb7b4cf1dc699
김아름 <rdf136@naver.com>
2024-10-31 16:56:03 +0900

    be/feat : QuerydslConfig 구현 (#33)

---
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/global/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package com.palja.audisay.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+/**
+ * jpaQueryFactory
+ */
+@Configuration
+public class QuerydslConfig {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
