62443564f01ac027826da98cbfb675b1794e15f6
안치숙 <songweddin@naver.com>
2024-10-31 21:42:50 +0900

    be/fix: memberId를 가져오는 util 함수 수정 (#27)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/global/util/SessionUtil.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/global/util/SessionUtil.java
@@ -1,17 +1,18 @@
 package com.palja.audisay.global.util;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.palja.audisay.domain.member.service.CustomUserDetails;
 import com.palja.audisay.global.exception.exceptions.MemberAccessDeniedException;
-import jakarta.servlet.http.HttpSession;
 
 public class SessionUtil {
 	// 세션에서 memberId를 가져오는 메서드
-	public static Long getMemberIdFromSession(HttpSession session) {
-		Long memberId = (Long) session.getAttribute("memberId");
-
-		if (memberId == null) {
-			// 로그인되지 않은 사용자면 'M005: 페이지에 접근할 수 있는 권한이 없습니다' 에러 던지기
+	public static Long getMemberId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails userDetails)) {
 			throw new MemberAccessDeniedException();
 		}
-		return memberId;
+		return userDetails.getId();
 	}
 }
