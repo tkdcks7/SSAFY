7d6ad17799623331661c181d3fb845669e9d606b
이예림 <yr5698@gmail.com>
2024-10-31 14:49:07 +0900

    be/fix: exception 부모 객체 수정

--- BE/Spring/audisay/src/main/java/com/palja/audisay/global/exception/exceptions/InvalidDatetimeException.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/global/exception/exceptions/InvalidDatetimeException.java
@@ -1,9 +1,9 @@
 package com.palja.audisay.global.exception.exceptions;
 
-import com.palja.audisay.global.exception.CustomException;
 import com.palja.audisay.global.exception.GlobalExceptionConstants;
+import com.palja.audisay.global.exception.InternalServerException;
 
-public class InvalidDatetimeException extends CustomException {
+public class InvalidDatetimeException extends InternalServerException {
 
 	private static final long serialVersionUID = 1L;
 
