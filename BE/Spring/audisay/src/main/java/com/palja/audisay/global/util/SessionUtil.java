ecad409b26a0399878eb428fb286b24cfa2711a8
안치숙 <songweddin@naver.com>
2024-10-31 17:56:00 +0900

    be/feat: 세션 로그인 시 memberId 반환하는 util 함수 추가 (#27)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/global/util/SessionUtil.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/global/util/SessionUtil.java
@@ -1,18 +1,17 @@
 package com.palja.audisay.global.util;
 
 import com.palja.audisay.global.exception.exceptions.MemberAccessDeniedException;
-
 import jakarta.servlet.http.HttpSession;
 
 public class SessionUtil {
-	// 세션에서 userId를 가져오는 메서드
-	public static Long getUserIdFromSession(HttpSession session) {
-		Long userId = (Long)session.getAttribute("userId");
+	// 세션에서 memberId를 가져오는 메서드
+	public static Long getMemberIdFromSession(HttpSession session) {
+		Long memberId = (Long) session.getAttribute("memberId");
 
-		if (userId == null) {
+		if (memberId == null) {
 			// 로그인되지 않은 사용자면 'M005: 페이지에 접근할 수 있는 권한이 없습니다' 에러 던지기
 			throw new MemberAccessDeniedException();
 		}
-		return userId;
+		return memberId;
 	}
 }
