ddd6d2938e75c54e780d8922fc3e31e2d5c96b5c
김아름 <rdf136@naver.com>
2024-11-01 03:31:44 +0900

    be/feat: 좋아요 추가/삭제 컨트롤러 메서드 구현 (#22)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/controller/BookController.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/controller/BookController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,23 +15,32 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/published-books")
-@Tag(name = "출판 도서", description = "출판 도서 검색 목록 / 상세 내용 조회 API")
+@Tag(name = "출판 도서", description = "출판 도서 검색 목록 / 상세 정보 관련 API")
 public class BookController {
 
 	private final BookService bookService;
 
-	@Operation(summary = "도서 상세 내용 조회", description = "bookId의 상세 내용() 반환")
+	@Operation(summary = "도서 상세 내용 조회",
+		description = "도서(bookId)의 상세 내용**(제목, 표지, 표지설명, 저자, 출판사, 카테고리(장르), 출판년도, 줄거리, ISBN, 자체TTS여부, 리뷰평점, 사용자 담은/좋아요 여부)** 반환")
 	@Parameters({
 		@Parameter(name = "bookId", description = "도서 식별 번호", required = true, example = "1")
 	})
 	@GetMapping("/{bookId}")
-	public ResponseEntity<?> getPublishedBookDetail(@PathVariable("bookId") Long bookId) {
+	public ResponseEntity<PublishedBookInfoDto> getPublishedBookDetail(@PathVariable("bookId") Long bookId) {
 		return new ResponseEntity<>(bookService.getPublishedBookDetail(bookId), HttpStatus.OK);
 	}
 
+	@Operation(summary = "좋아요 추가/삭제", description = "likedFlag = true : 좋아요 / likedFlag = false : 좋아요 해제")
+	@JsonView(MemberBookStatusReqDto.LikeView.class)
+	@PostMapping("/liked-books")
+	public ResponseEntity<?> updateLikeStatus(@Valid @RequestBody MemberBookStatusReqDto bookStatusReqDto) {
+		bookService.updateLikeStatus(bookStatusReqDto.getBookId(), bookStatusReqDto.getLikedFlag());
+		return ResponseEntity.ok().build();
+	}
 }
