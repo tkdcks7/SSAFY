eb2cb00d6ff0c24f358f9e18db8a22d1f194b686
안치숙 <songweddin@naver.com>
2024-10-31 17:55:22 +0900

    be/feat: 회원가입, 로그인, 이메일 체크 컨트롤러 서비스 추가 (#27)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/controller/MemberController.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/controller/MemberController.java
@@ -1,21 +1,17 @@
 package com.palja.audisay.domain.member.controller;
 
+import com.palja.audisay.domain.member.dto.EmailCheckRequestDto;
+import com.palja.audisay.domain.member.dto.MemberRegisterRequestDto;
+import com.palja.audisay.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.palja.audisay.domain.member.dto.EmailCheckRequestDto;
-import com.palja.audisay.domain.member.dto.MemberRegisterRequestDto;
-import com.palja.audisay.domain.member.service.MemberService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,15 +24,6 @@ public class MemberController {
 	// 회원가입
 	@PostMapping
 	@Operation(summary = "회원 가입", description = "입력 형식 맞춰서 회원가입 해야 함")
-	@Parameters({
-		@Parameter(name = "email", description = "이메일 형식에 맞춰야 함, 중복X", example = "ssafy@gmail.com"),
-		@Parameter(name = "password", description = "영어 대소문자, 숫자, 특수문자 가능, 8자리 이상", example = "ssafyssafy"),
-		@Parameter(name = "name", description = "특수문자X, 2~18자", example = "이름"),
-		@Parameter(name = "nickname", description = "특수문자X, 2~15자", example = "닉네임"),
-		@Parameter(name = "birth", description = "날짜 형식으로", example = "2000-01-01"),
-		@Parameter(name = "gender", description = "남자는 'M', 여자는 'F'", example = "M"),
-		@Parameter(name = "blindFlag", description = "true/false", example = "true")
-	})
 	public ResponseEntity<Void> registerMember(
 		@Valid @RequestBody MemberRegisterRequestDto memberRegisterRequestDto) {
 		memberService.registerMember(memberRegisterRequestDto);
@@ -45,6 +32,7 @@ public class MemberController {
 
 	// 이메일 중복 확인
 	@PostMapping("/email-check")
+	@Operation(summary = "이메일 중복 체크", description = "이메일 중복 체크 로직")
 	public ResponseEntity<Void> checkEmailDuplicate(@Valid @RequestBody EmailCheckRequestDto emailCheckRequestDto) {
 		memberService.validateDuplicateEmail(emailCheckRequestDto.getEmail());
 		return ResponseEntity.ok().build();
--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/service/MemberService.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/service/MemberService.java
@@ -1,25 +1,29 @@
 package com.palja.audisay.domain.member.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.palja.audisay.domain.member.dto.LoginRequestDto;
 import com.palja.audisay.domain.member.dto.MemberRegisterRequestDto;
 import com.palja.audisay.domain.member.entity.Member;
 import com.palja.audisay.domain.member.repository.MemberRepository;
 import com.palja.audisay.global.exception.exceptions.MemberEmailDuplicatedException;
-import com.palja.audisay.global.exception.exceptions.MemberNotFoundException;
-
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationManager authenticationManager;
 
 	@Transactional
 	public void registerMember(@Valid MemberRegisterRequestDto memberRegisterRequestDto) {
@@ -45,13 +49,19 @@ public class MemberService {
 	}
 
 	public Long authenticateUser(LoginRequestDto loginRequestDto) {
-		// 멤버 이메일로 있는지 확인
-		Member member = memberRepository.findByEmail(loginRequestDto.getEmail())
-			.orElseThrow(MemberNotFoundException::new);
-		// 비밀번호가 일치하는지 확인
-		if (!passwordEncoder.matches(loginRequestDto.getPassword(), member.getPassword())) {
-			throw new MemberNotFoundException();
-		}
-		return member.getMemberId();
+		// UsernamePasswordAuthenticationToken 생성
+		UsernamePasswordAuthenticationToken authToken =
+				new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+
+		// AuthenticationManager에 전달하여 인증 수행
+		Authentication authentication = authenticationManager.authenticate(authToken);
+
+		// 인증된 Authentication 객체를 SecurityContext에 저장
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		log.info("Authentication 저장 details: {}", authentication);
+
+		// SecurityContext에서 CustomUserDetails를 통해 memberId 추출
+		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+		return userDetails.getId();
 	}
 }
