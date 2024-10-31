4b3b24d41b41afba6e076546b7909709452b1f18
김아름 <rdf136@naver.com>
2024-11-01 06:34:38 +0900

    be/feat: LikeRepository 구현 (#22)
    1. existsByMemberAndBook
    - 회원의 기존 좋아요 존재 여부 확인 메서드
    
    2. findLikedBooksByMemberId
    - 회원이 좋아한 책 목록 조회 메서드
    
    3. deleteByMemberAndBook
    - 좋아요 삭제 메서드

---
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/likes/repository/LikesRepository.java
@@ -0,0 +1,27 @@
+package com.palja.audisay.domain.likes.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.palja.audisay.domain.book.entity.Book;
+import com.palja.audisay.domain.likes.entity.Likes;
+import com.palja.audisay.domain.member.entity.Member;
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
+	// 회원의 기존 좋아요 존재 여부 확인 메서드
+	boolean existsByMemberAndBook(Member member, Book book);
+
+	// 회원이 좋아한 책 목록 조회 메서드
+	@Query("""
+		SELECT b
+		FROM Likes l JOIN l.book b
+		WHERE l.member.memberId = :memberId
+		""")
+	List<Book> findLikedBooksByMemberId(@Param("memberId") Long memberId);
+
+	// 좋아요 삭제 메서드
+	void deleteByMemberAndBook(Member member, Book book);
+}
