ec6aa894a5ea22136236a7997ee3cc02f0f1d46f
김아름 <rdf136@naver.com>
2024-11-01 07:38:24 +0900

    be/feat: 도서 상세 정보 조회 서비스 메서드 구현 (#22)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/service/BookService.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/service/BookService.java
@@ -3,8 +3,10 @@ package com.palja.audisay.domain.book.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.palja.audisay.domain.book.dto.PublishedBookInfoDto;
 import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.book.repository.BookRepository;
+import com.palja.audisay.domain.member.service.MemberService;
 import com.palja.audisay.global.exception.exceptions.PublishedBookNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class BookService {
 
+	private final MemberService memberService;
 	private final BookRepository bookRepository;
+
+	// 도서 상세 정보 조회 메서드.
+	public PublishedBookInfoDto findPublishedBookDetail(Long memberId, Long bookId) {
+		// 사용자 검증
+		memberService.validateMember(memberId);
+		// bookId의 상세 내용 조회
+		// bookId의 리뷰 내용 조회
+		// 사용자 bookId 관련 정보(cartFlag, likeFlag) 조회
+		return bookRepository.findBookDetailByBookIdAndMemberId(memberId, bookId)
+			.orElseThrow(PublishedBookNotFoundException::new);
+	}
 
 	public Book validateBook(Long bookId) {
 		return bookRepository.findByBookId(bookId).orElseThrow(PublishedBookNotFoundException::new);
