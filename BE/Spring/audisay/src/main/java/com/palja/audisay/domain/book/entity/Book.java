1081ace5a76d55b14f26d18932a7859cca46e672
안치숙 <songweddin@naver.com>
2024-10-31 17:53:27 +0900

    be/fix: Book entity 수정 (#27)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/entity/Book.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/entity/Book.java
@@ -1,30 +1,14 @@
 package com.palja.audisay.domain.book.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
+import com.palja.audisay.domain.category.entity.Category;
+import com.palja.audisay.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 
-import com.palja.audisay.domain.category.entity.Category;
-import com.palja.audisay.domain.member.entity.Member;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "book")
@@ -78,7 +62,7 @@ public class Book {
 	@Column(columnDefinition = "text", nullable = false)
 	private String epub;
 
-	@Column(columnDefinition = "enum('PUBLISHED, REGISTERED')", nullable = false)
+	@Column(columnDefinition = "enum('PUBLISHED', 'REGISTERED')", nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Dtype dtype;
 
