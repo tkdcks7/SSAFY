110f505e1591cf0eb9d0e7833f28b0353d1a46de
이예림 <yr5698@gmail.com>
2024-10-31 18:00:02 +0900

    be/feat: swagger 적용 (#31)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/note/controller/NoteController.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/note/controller/NoteController.java
@@ -14,6 +14,9 @@ import com.palja.audisay.domain.note.dto.request.NoteRequestDto;
 import com.palja.audisay.domain.note.dto.response.NoteResponseDto;
 import com.palja.audisay.domain.note.service.NoteService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -23,6 +26,7 @@ public class NoteController {
 	private final NoteService noteService;
 
 	@GetMapping()
+	@Operation(summary = "독서노트 조회", description = "멤버ID로 독서노트 조회")
 	public ResponseEntity<NoteResponseDto> findByMember() {
 		long memberId = 1L;
 		NoteResponseDto noteResponseDto = noteService.findNotesByMember(memberId);
@@ -30,6 +34,10 @@ public class NoteController {
 	}
 
 	@GetMapping("/{bookId}")
+	@Operation(summary = "독서노트 조회(뷰어)", description = "멤버ID와 책ID로 독서노트 조회. 뷰어 표시용")
+	@Parameters({
+		@Parameter(name = "bookId", description = "책 ID (기본값: 1)", example = "1")
+	})
 	public ResponseEntity<NoteResponseDto> findByMemberAndBook(@PathVariable Long bookId) {
 		long memberId = 1L;
 		NoteResponseDto noteResponseDto = noteService.findNotesByMemberAndBook(memberId, bookId);
@@ -37,6 +45,7 @@ public class NoteController {
 	}
 
 	@PostMapping()
+	@Operation(summary = "독서노트 생성", description = "독서노트 생성")
 	public ResponseEntity<?> saveNote(@RequestBody NoteRequestDto noteRequestDto) {
 		long memberId = 1L;
 		boolean f = noteService.saveNote(memberId, noteRequestDto);
@@ -47,6 +56,10 @@ public class NoteController {
 	}
 
 	@DeleteMapping("/{noteId}")
+	@Operation(summary = "독서노트 삭제", description = "노트ID로 독서노트 삭제")
+	@Parameters({
+		@Parameter(name = "noteId", description = "노트 ID (기본값: 1)", example = "1")
+	})
 	public ResponseEntity<?> deleteByNoteId(@PathVariable Long noteId) {
 		long memberId = 1L;
 		noteService.deleteNoteByNoteId(memberId, noteId);
