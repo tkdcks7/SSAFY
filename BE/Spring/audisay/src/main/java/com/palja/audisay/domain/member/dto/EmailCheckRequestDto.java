3c602371e512f6fa67bdddebff5d828d44fb1b47
안치숙 <songweddin@naver.com>
2024-10-31 17:54:46 +0900

    be/feat: 회원가입, 로그인, 이메일 체크 dto 추가 (#27)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/dto/EmailCheckRequestDto.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/member/dto/EmailCheckRequestDto.java
@@ -1,5 +1,6 @@
 package com.palja.audisay.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -12,5 +13,6 @@ import lombok.Setter;
 public class EmailCheckRequestDto {
 	@NotBlank
 	@Email
+	@Schema(description = "이메일 형식에 맞춰야 함, 중복X", example = "ssafy@gmail.com", defaultValue = "ssafy@gmail.com")
 	private String email;
 }
