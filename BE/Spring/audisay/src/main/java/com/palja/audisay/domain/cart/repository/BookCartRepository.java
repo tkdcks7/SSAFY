73280e94aec23cbcb9acd465852ea17db82a4544
김아름 <rdf136@naver.com>
2024-11-04 00:50:37 +0900

    be/feat: 도서 담기 서비스 메서드 구현 (#35)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/cart/repository/BookCartRepository.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/cart/repository/BookCartRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.repository.query.Param;
 
 import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.cart.entity.BookCart;
+import com.palja.audisay.domain.cart.entity.BookCartId;
 import com.palja.audisay.domain.member.entity.Member;
 
-public interface BookCartRepository extends JpaRepository<BookCart, Long> {
+public interface BookCartRepository extends JpaRepository<BookCart, BookCartId> {
 
 	// 회원의 기존 담은 도서 존재 여부 확인 메서드
 	boolean existsByMemberAndBook(Member member, Book book);
