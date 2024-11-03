a010e42238cb66056e0c42e4de75d832946f3a63
이예림 <yr5698@gmail.com>
2024-11-04 00:25:51 +0900

    be/feat: 추천 도서 조회 - 연령대 인기 (#42)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/controller/RecommendationController.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/controller/RecommendationController.java
@@ -27,4 +27,36 @@ public class RecommendationController {
 		Long memberId = 1L;
 		return new ResponseEntity<>(recommendationService.getFamousBooks(), HttpStatus.OK);
 	}
+
+	@Operation(summary = "연령대 인기 도서 조회",
+		description = "연령대 인기 도서 목록 조회 ")
+	@GetMapping("/demographics")
+	public ResponseEntity<RecommendationBookDto> getDemographicsPopularBooks() {
+		Long memberId = 1L;
+		return new ResponseEntity<>(recommendationService.getDemographicsBooks(memberId), HttpStatus.OK);
+	}
+
+	@Operation(summary = "카테고리 인기 도서 조회",
+		description = "선호 카테고리 인기 도서 목록 조회 ")
+	@GetMapping("/favorite-category")
+	public ResponseEntity<RecommendationBookDto> getCategoryPopularBooks() {
+		Long memberId = 1L;
+		return new ResponseEntity<>(recommendationService.getFamousBooks(), HttpStatus.OK);
+	}
+
+	@Operation(summary = "최근 유사 도서 조회",
+		description = "가장 최근에 본 도서와 유사한 도서 목록 조회 ")
+	@GetMapping("/recent")
+	public ResponseEntity<RecommendationBookDto> getSimilarBooks() {
+		Long memberId = 1L;
+		return new ResponseEntity<>(recommendationService.getFamousBooks(), HttpStatus.OK);
+	}
+
+	@Operation(summary = "유사한 유저 선호 도서 조회",
+		description = "유사한 유저 선호 도서 목록 조회 ")
+	@GetMapping("/similar-members")
+	public ResponseEntity<RecommendationBookDto> getSimilarMemberBooks() {
+		Long memberId = 1L;
+		return new ResponseEntity<>(recommendationService.getFamousBooks(), HttpStatus.OK);
+	}
 }
--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/service/RecommendationService.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/service/RecommendationService.java
@@ -1,27 +1,34 @@
 package com.palja.audisay.domain.recommendation.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.palja.audisay.domain.book.dto.PublishedBookInfoDto;
 import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.book.repository.BookRepository;
+import com.palja.audisay.domain.member.entity.Gender;
+import com.palja.audisay.domain.member.entity.Member;
 import com.palja.audisay.domain.member.repository.MemberRepository;
 import com.palja.audisay.domain.recommendation.dto.response.RecommendationBookDto;
 import com.palja.audisay.domain.recommendation.entity.Criterion;
+import com.palja.audisay.domain.recommendation.entity.DemographicsBook;
 import com.palja.audisay.domain.recommendation.entity.FamousBook;
 import com.palja.audisay.domain.recommendation.repository.CategoryBookRepository;
 import com.palja.audisay.domain.recommendation.repository.DemographicsBookRepository;
 import com.palja.audisay.domain.recommendation.repository.FamousBookRepository;
 import com.palja.audisay.domain.recommendation.repository.SimilarBookRepository;
 import com.palja.audisay.domain.recommendation.repository.SimilarMemberBookRepository;
+import com.palja.audisay.global.exception.exceptions.MemberNotFoundException;
 import com.palja.audisay.global.exception.exceptions.RecommendationNotFoundException;
 import com.palja.audisay.global.util.ImageUtil;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class RecommendationService {
 	private final MemberRepository memberRepository;
@@ -35,15 +42,51 @@ public class RecommendationService {
 	// util
 	private final ImageUtil imageUtil;
 
+	// 인기 도서 조회
 	public RecommendationBookDto getFamousBooks() {
 		// 1. 최신 인기 도서 조회 (mongoDB)
 		FamousBook famousBook = famousBookRepository.findFirstByOrderByGroupIdDesc();
 		if (famousBook == null) {
 			throw new RecommendationNotFoundException();
 		}
-		// 2. 테마 상세 정보 조회
+		// 2. 도서 상세 정보 조회
 		List<Book> bookList = bookRepository.findByBookIdIn(famousBook.getBookList());
-		List<PublishedBookInfoDto> publishedBookInfoDtoList = bookList.stream().map(book ->
+		List<PublishedBookInfoDto> publishedBookInfoDtoList = bookToDto(bookList);
+
+		return RecommendationBookDto.builder()
+			.bookList(publishedBookInfoDtoList)
+			.criterion(Criterion.FAMOUS_BOOK.format())
+			.build();
+	}
+
+	// 연령대 인기 도서 조회
+	public RecommendationBookDto getDemographicsBooks(Long memberId) {
+		// 1. 유저 정보 조회
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(MemberNotFoundException::new);
+		String groupId = String.format("%d_%d",
+			(LocalDate.now().getYear() - member.getBirth().getYear()) / 10 * 10,
+			member.getGender() == Gender.M? 1 : 0);
+		String totalGroupId = "Total";
+		// 2. 연령대 인기 도서 조회 (mongoDB)
+		DemographicsBook demographicsBook = demographicsBookRepository.findByGroupId(groupId);
+		if (demographicsBook == null) {
+			demographicsBook = demographicsBookRepository.findByGroupId(totalGroupId);
+		}
+		// 3. 도서 상세 정보 조회
+		List<Book> bookList = bookRepository.findByBookIdIn(demographicsBook.getBookList());
+		List<PublishedBookInfoDto> publishedBookInfoDtoList = bookToDto(bookList);
+
+		return RecommendationBookDto.builder()
+			.bookList(publishedBookInfoDtoList)
+			.criterion(Criterion.FAMOUS_BOOK.format())
+			.build();
+	}
+
+	// entity -> dto 변환
+	// Dto 안에 넣으려다 ImageUtil이 있어 Service 단에서 처리
+	public List<PublishedBookInfoDto> bookToDto(List<Book> bookList) {
+		return bookList.stream().map(book ->
 			PublishedBookInfoDto.builder()
 				.bookId(book.getBookId())
 				.coverAlt(book.getCoverAlt())
@@ -53,28 +96,6 @@ public class RecommendationService {
 				.story(book.getStory())
 				.cover(imageUtil.getFullImageUrl(book.getCover()))
 				.build()).toList();
-
-		return RecommendationBookDto.builder()
-			.bookList(publishedBookInfoDtoList)
-			.criterion(Criterion.FAMOUS_BOOK.format())
-			.build();
 	}
-/*
-						"bookId" : 3,
-						"cover" : "s3 주소",
-						"coverAlt" : "아기돼지가 3마리 있다.",
-						"title" : "아기돼지 삼형제",
-						"author" : "미상",
-						"publisher" : "한빛미디어",
-						"story" : "돼지 3마리가 집을 짓는 이야기"
- */
-	/*
-	// 1. 유저 정보 조회
-		Member member = memberRepository.findById(memberId)
-			.orElseThrow(MemberNotFoundException::new);
-	String groupId = String.format("%d_%d",
-			(LocalDate.now().getYear() - member.getBirth().getYear()) / 10 * 10,
-			member.getGender() == Gender.M? 1 : 0);
-		String totalGroupId = "Total";
-	 */
+
 }
