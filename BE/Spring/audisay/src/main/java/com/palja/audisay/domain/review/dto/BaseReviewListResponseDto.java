bf077f8588f9d49a8fc9d5cf958fa2bdd99650a8
안치숙 <songweddin@naver.com>
2024-11-04 16:32:43 +0900

    be/fix: 커서 기반 리스트 응답 dto 변수 수정 (#43)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/dto/BaseReviewListResponseDto.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/dto/BaseReviewListResponseDto.java
@@ -1,13 +1,15 @@
 package com.palja.audisay.domain.review.dto;
 
-import java.util.List;
-
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @SuperBuilder // Lombok의 @SuperBuilder를 사용하여 상속받은 클래스에서도 빌더 패턴 사용
 public class BaseReviewListResponseDto {
 	private List<ReviewResponseDto> reviewList;
-	private String nextCursor;
+	private LocalDateTime lastUpdatedAt;
+	private Long lastReviewId;
 }
