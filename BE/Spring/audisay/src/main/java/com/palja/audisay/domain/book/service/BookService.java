91fff21e4be79c8f92ce66357fdcec285e01a7e1
김아름 <rdf136@naver.com>
2024-11-04 14:12:56 +0900

    be/feat: 도서 목록 조회 서비스 메서드 구현 (#44)
    1. 도서 목록 조회 서비스 메서드
    - cursor based pagination
    - like 비교 조회

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/service/BookService.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/service/BookService.java
@@ -1,9 +1,15 @@
 package com.palja.audisay.domain.book.service;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.palja.audisay.domain.book.dto.PublishedBookInfoDto;
+import com.palja.audisay.domain.book.dto.LastBookInfo;
+import com.palja.audisay.domain.book.dto.request.BookSearchReqDto;
+import com.palja.audisay.domain.book.dto.respose.BookSearchResDto;
+import com.palja.audisay.domain.book.dto.respose.PublishedBookInfoDto;
 import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.book.entity.Dtype;
 import com.palja.audisay.domain.book.repository.BookRepository;
@@ -41,6 +47,58 @@ public class BookService {
 		publishedBookInfoDto.setPublishedAt(publishedStr);
 		publishedBookInfoDto.setCover(fullCoverUrl);
 		return publishedBookInfoDto;
+	}
+
+	public BookSearchResDto getSearchPublishedBookResult(Long memberId, BookSearchReqDto bookSearchReqDto) {
+		// 사용자 검증
+		memberService.validateMember(memberId);
+
+		List<Book> bookRawList = bookRepository.searchBookList(bookSearchReqDto);
+
+		if (bookRawList.isEmpty()) {
+			return BookSearchResDto.builder()
+				.bookList(Collections.emptyList())
+				.build();
+		}
+
+		// 도서 목록 후처리
+		LastBookInfo lastBookInfo = processLastBookInfo(bookRawList, bookSearchReqDto.getPageSize());
+
+		List<PublishedBookInfoDto> bookResultList = bookRawList.stream()
+			.map(book -> PublishedBookInfoDto.builder()
+				.bookId(book.getBookId())
+				.title(book.getTitle())
+				.cover(imageUtil.getFullImageUrl(book.getCover()))  // 이미지 URL 접두사 추가
+				.coverAlt(book.getCoverAlt())
+				.author(book.getAuthor())
+				.dtype(book.getDtype())
+				.publisher(book.getPublisher())
+				.publishedAt(StringUtil.dateToString(book.getPublishedDate()))
+				.build())
+			.toList();
+
+		return BookSearchResDto.builder()
+			.keyword(bookSearchReqDto.getKeyword())
+			.bookList(bookResultList)
+			.lastCreatedAt(lastBookInfo.getLastCreatedAt())
+			.lastBookId(lastBookInfo.getLastBookId())
+			.build();
+	}
+
+	/**
+	 * 조회한 목록에서 마지막 도서 정보 추출 메서드.
+	 *
+	 * @param bookRawList DB에서 추출한 원본 리스트.
+	 * @param pageSize 요청한 페이지 크기.
+	 * @return
+	 */
+	private LastBookInfo processLastBookInfo(List<Book> bookRawList, int pageSize) {
+		boolean hasNext = bookRawList.size() > pageSize;
+		if (hasNext) {
+			bookRawList.removeLast();  // 마지막 도서 제거
+		}
+		Book lastBook = hasNext ? bookRawList.getLast() : null;
+		return lastBook != null ? new LastBookInfo(lastBook.getBookId(), lastBook.getCreatedAt()) : new LastBookInfo();
 	}
 
 	public Book validatePublishedBook(Long bookId) {
