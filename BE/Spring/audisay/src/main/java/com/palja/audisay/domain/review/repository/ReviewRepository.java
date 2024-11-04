4d03cef58fd98605a628caf8d928596b203531b4
안치숙 <songweddin@naver.com>
2024-11-04 16:33:33 +0900

    be/feat: querydsl 이용하여 커서 기반 리뷰 목록 조회 (#43)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/repository/ReviewRepository.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/repository/ReviewRepository.java
@@ -1,26 +1,13 @@
 package com.palja.audisay.domain.review.repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.member.entity.Member;
 import com.palja.audisay.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 	boolean existsByMemberAndBook(Member member, Book book); // 중복 리뷰 확인 메서드
 
 	int deleteByReviewIdAndMemberMemberId(Long reviewId, Long memberId);
 
-	List<Review> findByMemberAndUpdatedAtLessThanOrderByUpdatedAtDesc(
-		Member member, LocalDateTime updatedAt, Pageable pageable);
-
-	Optional<Review> findByMemberAndBook(Member member, Book book);
-
-	List<Review> findByBookAndMemberNotAndUpdatedAtLessThanOrderByUpdatedAtDesc(Book book, Member member,
-		LocalDateTime updatedAt, Pageable pageable);
 }
