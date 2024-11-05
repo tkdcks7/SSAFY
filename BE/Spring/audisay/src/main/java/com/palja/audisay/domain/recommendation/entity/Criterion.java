415f7cc50443f6fb184e4c92e2f764e10ae4ef10
이예림 <yr5698@gmail.com>
2024-11-05 15:22:26 +0900

    be/fix: 유사 도서 criterion 텍스트 수정 (#57)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/entity/Criterion.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/entity/Criterion.java
@@ -9,7 +9,7 @@ public enum Criterion {
 	FAMOUS_BOOK("인기 도서"),
 	DEMOGRAPHICS_BOOK("%s 인기 도서"),
 	CATEGORY_BOOK("%s 카테고리 인기 도서"),
-	SIMILAR_BOOK("최근 본 %s와/과 유사한 도서"),
+	SIMILAR_BOOK("최근 본 %s와/과 비슷한 도서"),
 	SIMILAR_MEMBER_BOOK("나와 비슷한 사용자가 좋아한 도서");
 
 	private final String template;
