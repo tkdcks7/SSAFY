ddff14ebc61196e00e8206672f1c3ac21b9b2e29
이예림 <yr5698@gmail.com>
2024-11-05 15:26:15 +0900

    be/feat: 도서 상세 추천 criterion 추가

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/entity/Criterion.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/entity/Criterion.java
@@ -10,7 +10,9 @@ public enum Criterion {
 	DEMOGRAPHICS_BOOK("%s 인기 도서"),
 	CATEGORY_BOOK("%s 카테고리 인기 도서"),
 	SIMILAR_BOOK("최근 본 %s와/과 비슷한 도서"),
-	SIMILAR_MEMBER_BOOK("나와 비슷한 사용자가 좋아한 도서");
+	SIMILAR_MEMBER_BOOK("나와 비슷한 사용자가 좋아한 도서"),
+	SIMILAR_BOOK_BY_CONTEXT("이 도서와 비슷한 도서"),
+	SIMILAR_BOOK_BY_LIKES("이 도서를 좋아한 사용자들이 좋아한 도서");
 
 	private final String template;
 
