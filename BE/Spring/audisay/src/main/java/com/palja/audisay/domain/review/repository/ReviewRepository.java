4c934493b4ed7a7cd3e8c192ea86af590af779ef
안치숙 <songweddin@naver.com>
2024-11-05 15:55:00 +0900

    be/fix: 리뷰 수정하기 로직 수정 (#52)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/repository/ReviewRepository.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/review/repository/ReviewRepository.java
@@ -4,9 +4,14 @@ import com.palja.audisay.domain.book.entity.Book;
 import com.palja.audisay.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 
 	int deleteByReviewIdAndMemberMemberId(Long reviewId, Long memberId);
 
 	boolean existsByMemberMemberIdAndBook(Long memberId, Book book);
+
+	Optional<Review> findByReviewIdAndMemberMemberId(Long reviewId, Long memberId);
+
 }
