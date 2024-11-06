d276e5972c3f90898a895f599f79af8160b46597
김아름 <rdf136@naver.com>
2024-11-06 17:01:24 +0900

    be/feat: Builder 추가 (#60)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/viewLog/entity/ViewLog.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/viewLog/entity/ViewLog.java
@@ -3,19 +3,20 @@ package com.palja.audisay.domain.viewLog.entity;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Document(collection = "log_bookView")
 @Getter
+@Builder
 @ToString
 public class ViewLog {
-
 	@Id
 	private String id;
 	private Long memberId;
 	private Long bookId;
 	private String categoryId;
-	private String createdAt;
 	private String title;
+	private String createdAt;
 }
