3dc62210c41ea0d88b9c30ac27b11b2ce9e02f61
이예림 <yr5698@gmail.com>
2024-11-06 14:07:29 +0900

    be/refactor: mongodb repository 구분하도록 어노테이션 추가 (#64)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/repository/RecommendationStringRepository.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/recommendation/repository/RecommendationStringRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.palja.audisay.domain.recommendation.entity.RecommendationString;
+import com.palja.audisay.global.annotation.MongoDBRepository;
 
 @Repository
+@MongoDBRepository
 public interface RecommendationStringRepository extends
 	MongoRepository<RecommendationString, String> {
 
