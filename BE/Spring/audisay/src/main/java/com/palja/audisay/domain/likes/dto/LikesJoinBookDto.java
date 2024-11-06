d1551aa11a31400ebcbc465ba433e627c37e141b
김아름 <rdf136@naver.com>
2024-11-06 09:07:56 +0900

    be/feat: Likes 테이블과 Book 테이블 조인 DTO (#47)

---
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/likes/dto/LikesJoinBookDto.java
@@ -0,0 +1,34 @@
+package com.palja.audisay.domain.likes.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.palja.audisay.domain.book.entity.Dtype;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class LikesJoinBookDto {
+	private Long bookId;
+	private String title;
+	private String cover;
+	private String coverAlt;
+	private String category;
+	private String author;
+	private String publisher;
+	private Dtype dtype;
+	@JsonIgnore
+	private LocalDateTime createdAtLike;
+	@JsonIgnore
+	private String coverRaw;
+}
