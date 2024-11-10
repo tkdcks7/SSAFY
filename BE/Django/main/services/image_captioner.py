92471646843dda135213d0a08bbc44fde50fe310
이예림 <yr5698@gmail.com>
2024-11-11 01:58:56 +0900

    be/fix: openai 모델 변경 (#70)

--- BE/Django/main/services/image_captioner.py
+++ BE/Django/main/services/image_captioner.py
@@ -1,7 +1,7 @@
 import ebooklib
 from ebooklib import epub
 from .epub_reader import EpubReader
-from .image_caption_util import AzureImageAnalysis, OpenAIAnalyzer
+from .image_caption_util import AzureImageAnalysis, OpenAIAnalysis
 
 class ImageCaptioner:
     # staticmethod를 이용하여 self 인자를 전달하지 않도록 한다 (정적 메소드)
@@ -17,7 +17,7 @@ class ImageCaptioner:
             processed_images.append((im.file_name, azure_result.caption.text, im.get_content()))
 
         # 3. 이미지 파일을 openai로 이미지 캡셔닝
-        open_ai_analyzer = OpenAIAnalyzer()
+        open_ai_analyzer = OpenAIAnalysis()
         openai_result = open_ai_analyzer.analyze_openai_image(processed_images)
 
         # 4. 추가된 캡션을 이미지에 추가 
