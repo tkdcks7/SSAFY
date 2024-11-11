4d7bef83b49fbf79da22b860ff2c1ec44d6061e9
김아름 <rdf136@naver.com>
2024-11-11 11:16:14 +0900

    be/feat: ElasticSearch Document 구현 (#73)

---
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/entity/BookIndex.java
@@ -0,0 +1,65 @@
+package com.palja.audisay.domain.book.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Document(indexName = "books_index")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class BookIndex {
+	@Id
+	@Field(type = FieldType.Long)
+	private Long bookId;
+
+	@Field(type = FieldType.Text, analyzer = "korean_index", searchAnalyzer = "korean_search")
+	private String author;
+
+	@MultiField(mainField = @Field(type = FieldType.Text), otherFields = {
+		@InnerField(suffix = "keyword", type = FieldType.Keyword, ignoreAbove = 256)
+	})
+	private String category;
+
+	@MultiField(mainField = @Field(type = FieldType.Text), otherFields = {
+		@InnerField(suffix = "keyword", type = FieldType.Keyword, ignoreAbove = 256)
+	})
+	private String cover;
+
+	@MultiField(mainField = @Field(type = FieldType.Text), otherFields = {
+		@InnerField(suffix = "keyword", type = FieldType.Keyword, ignoreAbove = 256)
+	})
+	private String coverAlt;
+
+	@MultiField(mainField = @Field(type = FieldType.Text), otherFields = {
+		@InnerField(suffix = "keyword", type = FieldType.Keyword, ignoreAbove = 256)
+	})
+	private String dType;
+
+	@Field(type = FieldType.Date)
+	private LocalDateTime lastUpdated;
+
+	@Field(type = FieldType.Date)
+	private LocalDateTime publishedDate;
+
+	@Field(type = FieldType.Text, analyzer = "korean_index", searchAnalyzer = "korean_search")
+	private String publisher;
+
+	@Field(type = FieldType.Float)
+	private Float review;
+
+	@Field(type = FieldType.Text, analyzer = "korean_index", searchAnalyzer = "korean_search")
+	private String title;
+}
