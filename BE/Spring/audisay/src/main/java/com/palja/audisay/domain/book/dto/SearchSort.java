83dc9f81f0fc387ef4cd2e487a1ed08d97f9fb31
김아름 <rdf136@naver.com>
2024-11-12 01:54:13 +0900

    be/refactor: Map을 통해 입력에 대한 유효성 검사 및 반환 (#73)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/dto/SearchSort.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/dto/SearchSort.java
@@ -1,18 +1,27 @@
 package com.palja.audisay.domain.book.dto;
 
+import java.util.Map;
+
 import org.springframework.data.domain.Sort;
 
 public record SearchSort(
 	String sortBy,
 	Sort.Direction sortOrder
 ) {
+	// 정적 필드명을 매핑하는 맵
+	private static final Map<String, String> FIELD_MAP = Map.of(
+		"published_date", "publishedDate",
+		"title", "title.keyword"
+	);
+
 	public static SearchSort setSort(String sortBy, String sortOrder) {
+		// sortOrder 문자열에 따라 방향 설정 (기본값: ASC)
 		Sort.Direction direction = "desc".equalsIgnoreCase(sortOrder)
 			? Sort.Direction.DESC
 			: Sort.Direction.ASC;
 
-		// 필드명 변환 (published_date -> publishedDate)
-		String dbField = sortBy.equals("published_date") ? "publishedDate" : "title.keyword";
+		// sortBy에 대한 유효성 검사 및 변환
+		String dbField = FIELD_MAP.getOrDefault(sortBy.toLowerCase(), "publishedDate");
 		return new SearchSort(dbField, direction);
 	}
 }
