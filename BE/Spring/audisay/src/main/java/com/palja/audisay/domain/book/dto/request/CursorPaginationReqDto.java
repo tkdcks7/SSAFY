f9737c7a0c72ac3e70a0fb272c0585d69d52e023
김아름 <rdf136@naver.com>
2024-11-12 01:56:39 +0900

    be/feat: 커서 기반 페이지네이션의 공통(핵심) 필드로만 구성 (#73)

--- BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/dto/request/CursorPaginationReqDto.java
+++ BE/Spring/audisay/src/main/java/com/palja/audisay/domain/book/dto/request/CursorPaginationReqDto.java
@@ -10,18 +10,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+/**
+ * 커서 기반 페이지네이션에 사용될 cursorId 와 page size.
+ */
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
 public class CursorPaginationReqDto {
-	private LocalDateTime lastDateTime;  // 커서로 사용할 마지막 생성 시간
-	private Long lastId;              // 같은 시간에 생성된 경우를 위한 보조 커서
+	private LocalDateTime lastDateTime;    // 커서로 사용할 마지막 생성 시간
+	private Long lastId;                // 마지막 식별 번호 or 같은 시간에 생성된 경우를 위한 보조 커서
+	private String lastSearchId;        // 마지막 검색 결과의 식별번호
 	@Min(1)
 	@Builder.Default
 	private Integer pageSize = 10;
-
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize == null ? 10 : pageSize;
 	}
