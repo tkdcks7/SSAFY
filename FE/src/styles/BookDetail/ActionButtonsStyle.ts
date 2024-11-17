fe90069de1e376ef47e9518c90207594e460b7a1
안치숙 <songweddin@naver.com>
2024-11-17 10:23:53 +0900

    fe/feat: 다운로드 불가능한 경우 아이콘 색, 투명도 조절 (#125)

--- FE/src/styles/BookDetail/ActionButtonsStyle.ts
+++ FE/src/styles/BookDetail/ActionButtonsStyle.ts
@@ -1,6 +1,6 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 const responsiveFontSize = (factor: number) => width * (factor / 100);
 const responsiveWidth = (factor: number) => width * (factor / 100);
 const responsiveHeight = (factor: number) => height * (factor / 100);
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: responsiveHeight(1),
-    paddingHorizontal: responsiveWidth(5),
+    marginRight: responsiveWidth(1.5),
   },
   buttonWrapper: {
     flex: 6,
@@ -30,6 +30,10 @@ const styles = StyleSheet.create({
     width: responsiveWidth(15),
     height: responsiveWidth(15),
     marginBottom: responsiveFontSize(2),
+  },
+  disabledIcon: {
+    tintColor: '#A9A9A9', // 회색으로 변경하여 비활성화 상태 표시
+    opacity: 0.1, // 불투명도 낮춰 더 흐릿하게
   },
 });
 
